8066a7666a5d6ce52d60163d9ce8ac3ab3c9c53a
apryamostanov <anton.pryamostanov@gmail.com>
2019-02-20 14:14:33 +0400

    Reformat code

--- src/test/groovy/io/infinite/carburetor/tests/SpringBootAppTest.groovy
+++ src/test/groovy/io/infinite/carburetor/tests/SpringBootAppTest.groovy
@@ -2,15 +2,11 @@ package io.infinite.carburetor.tests
 
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.ApplicationContext
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.util.ClassUtils
 
 @RunWith(SpringRunner.class)
 class SpringBootAppTest {
-    @Autowired
-    private ApplicationContext context
 
     @Test
     void test() {
